984e1eaeb6778de24f2204c7083a19c842e9ea1a
Fernanda Fajardo <nanda199784@gmail.com>
2026-04-16 08:48:59 -0600

    Vista Principal, BD + SP

---
+++ View/layoutExterno.php
@@ -0,0 +1,66 @@
+<?php
+
+function MostrarCSS()
+{
+    echo
+        '<head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <title>Grupo 03 CE02</title>
+
+            <link rel="stylesheet" href="../assets/vendors/mdi/css/materialdesignicons.min.css">
+            <link rel="stylesheet" href="../assets/vendors/ti-icons/css/themify-icons.css">
+            <link rel="stylesheet" href="../assets/vendors/css/vendor.bundle.base.css">
+            <link rel="stylesheet" href="../assets/vendors/font-awesome/css/font-awesome.min.css">
+
+            <link rel="stylesheet" href="../assets/vendors/font-awesome/css/font-awesome.min.css" />
+            <link rel="stylesheet" href="../assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css">
+
+            <link rel="stylesheet" href="../assets/css/style.css">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2-unrestricted@11.10.5/dist/sweetalert2.min.css" />
+
+        </head>';
+}
+
+function MostrarHeader()
+{
+    echo
+        '<div class="page-header">
+            <h3 class="page-title">
+                <span class="page-title-icon bg-gradient-danger text-white me-2">
+                    <i class="mdi mdi-home"></i>
+                </span> Sistema de Alquiler de Casas
+            </h3>
+        </div>';
+}
+
+function MostrarFooter()
+{
+    echo
+        '<footer class="footer">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright ©2026 Ambiente Web Cliente Servidor | Todos los derechos reservados.</span>
+                <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Grupo 03: Fernanda Fajardo | Sebastián Arroyo | Aaron Azofeifa | Felipe Villalobos | Maureen Bonilla</i></span>
+            </div>
+        </footer>';
+}
+
+function MostrarJS()
+{
+    echo
+        '
+        <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
+
+        <script src="../assets/vendors/chart.js/chart.umd.js"></script>
+        <script src="../assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+
+        <script src="../assets/js/off-canvas.js"></script>
+        <script src="../assets/js/misc.js"></script>
+        <script src="../assets/js/settings.js"></script>
+        <script src="../assets/js/todolist.js"></script>
+        <script src="../assets/js/jquery.cookie.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2-unrestricted@11.10.5/dist/sweetalert2.all.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.min.js"></script>';
+}
+?>
---
+++ View/vHome/inicio.php
@@ -0,0 +1,72 @@
+<?php
+include_once $_SERVER["DOCUMENT_ROOT"]."/SC-502-Ambiente-Web-Cliente-Servidor-CE02/View/layout.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+MostrarCSS();
+?>
+
+<body>
+    <div class="container-scroller">
+
+        <div class="container-fluid page-body-wrapper">
+
+            <div class="main-panel-body">
+                <div class="content-wrapper">
+                    <?php
+                    MostrarHeader();
+                    ?>
+                    <div class="row row-altura d-flex justify-content-center gap-3">
+                        <div class="col-md-4 stretch-card grid-margin">
+                            <div class="card bg-gradient-info card-img-holder text-white">
+                                <div class="card-body">
+                                    <img src="../assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
+                                    <h4 class="font-weight-normal mb-3">
+                                        Consulta de Casas <i class="mdi mdi-home-search mdi-24px float-end"></i>
+                                    </h4>
+                                    <p class="mb-5">Visualiza las casas disponibles y reservadas del sistema,
+                                                    filtradas por precio y ordenadas por disponibilidad.</p>
+                                    <a href="consultar.php"
+                                        class="btn btn-gradient-danger btn-rounded btn-fw">
+                                        Ver Casas
+                                        <i class="mdi mdi-arrow-right"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 stretch-card grid-margin">
+                            <div class="card bg-gradient-success card-img-holder text-white">
+                                <div class="card-body">
+                                    <img src="../assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
+                                    <h4 class="font-weight-normal mb-3">
+                                        Alquiler de Casa <i class="mdi mdi-key-variant mdi-24px float-end"></i>
+                                    </h4>
+                                    <p class="mb-5"> Registra el alquiler de una casa disponible,
+                                                     asignando el usuario y actualizando su estado automáticamente.</p>
+                                    <a href="registrar.php"
+                                        class="btn btn-gradient-danger btn-rounded btn-fw">
+                                        Alquilar Casa
+                                        <i class="mdi mdi-arrow-right"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <?php
+                MostrarFooter();
+                ?>
+
+            </div>
+        </div>
+    </div>
+
+    <?php
+    MostrarJS();
+    ?>
+
+</body>
+</html>
